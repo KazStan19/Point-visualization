9a1c3ddb11f832ebe3b73162e4d014a1487c7589
KazStan19 <kazi.stan16@gmail.com>
2022-03-16 14:45:00 +0100

    all defects are clickable

--- src/components/area.jsx
+++ src/components/area.jsx
@@ -1,10 +1,11 @@
-import React,{useRef,useEffect,useContext} from 'react'
+import React,{useRef,useEffect,useContext,useState} from 'react'
 import {ThemeContext} from './home'
 
 export const Area = (props) => {
     
     const canvasRef = useRef(null)
     const contextRef = useRef(null)
+    const [first, setfirst] = useState([])
     const {width,height,scale,contours,pic} = useContext(ThemeContext)
 
 
@@ -19,39 +20,55 @@ export const Area = (props) => {
         canvas.style.zIndex = '97'
 
         const context = canvas.getContext("2d")
-        canvas.addEventListener('click',(e)=>{console.log("hello")})
-
+     
         let color = 'red'
         let opacity = 0.8
+        let num = -1
+        let name = []
 
         contours.forEach(item=>{
         
         if(item.name === pic.name && pic !== ''){
-            context.beginPath()
+            
+            name.push({name: new Path2D()})
+            num+=1
+            context.beginPath(name[num].name)
             context.fillStyle = color
             context.strokeStyle = color
             context.globalAlpha = opacity
 
-        context.moveTo(item.coords.coordinates[0][0] * scale,item.coords.coordinates[0][1] * scale)
+        name[num].name.moveTo(item.coords.coordinates[0][0] * scale,item.coords.coordinates[0][1] * scale)
         
         for(let i=1;i < item.coords.coordinates.length;i++){
 
-            context.lineTo(item.coords.coordinates[i][0] * scale,item.coords.coordinates[i][1] * scale)
+            name[num].name.lineTo(item.coords.coordinates[i][0] * scale,item.coords.coordinates[i][1] * scale)
             context.fillRect(item.coords.coordinates[i][0] * scale-5, item.coords.coordinates[i][1] * scale-5, 10, 10);
             
         
-        context.stroke()
+        context.stroke(name[num].name)
         }}
         context.fillStyle = 'rgba(255, 255, 255, 0)' 
-        context.fill()
-        
-        
-        
+        context.fill(name[num].name)
+        setfirst(name)
     
     })
         contextRef.current = context
     }, [pic,contours,scale])
     
+    if(document.getElementById("canvas") !==null){document.getElementById("canvas").addEventListener('click', function(event) {
+        event = event || window.event;
+        var ctx = document.getElementById("canvas").getContext("2d")
+        
+        for (var i = first.length - 1; i >= 0; i--){  
+        
+        if (first[i].name && ctx.isPointInPath(first[i].name, event.offsetX, event.offsetY)) {
+        
+            alert(event.clientX + ' : ' + event.clientY)
+
+        return
+        } 
+    }
+    })}
 
     return (
     <canvas id='canvas'  ref={canvasRef}></canvas>
