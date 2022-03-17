5d365299e228ac740cf892735498a99beb3244b1
KazStan19 <kazi.stan16@gmail.com>
2022-03-17 11:52:35 +0100

    all defects are clickable

--- src/components/area.jsx
+++ src/components/area.jsx
@@ -8,6 +8,49 @@ export const Area = (props) => {
     const [first, setfirst] = useState([])
     const {width,height,scale,contours,pic} = useContext(ThemeContext)
 
+    function addEvent(event) {
+        event = event || window.event;
+        var ctx = document.getElementById("canvas").getContext("2d")
+
+        let clickPlace = [(event.clientX - event.target.offsetLeft)/scale ,(event.clientY - event.target.offsetTop)/scale]
+        
+        for (var i = first.length - 1; i >= 0; i--){
+
+            let minMax = findMinMax(first[i].coords)
+
+        
+            if(clickPlace[0] >= minMax.x[0] && clickPlace[0] <= minMax.x[1] && clickPlace[1] >= minMax.y[0] && clickPlace[1] <= minMax.y[1] ){
+
+                alert(clickPlace[0] + " ; " + clickPlace[1])
+                console.log(clickPlace[0] + " ; " + clickPlace[1])
+
+                return;
+            }
+        
+        
+    }
+
+    canvasRef.current.removeEventListener('click',addEvent,true)
+
+    }
+
+    
+    function findMinMax(arr) {
+        let minY = arr[0][1], maxY = arr[0][1];
+        let minX = arr[0][0], maxX = arr[0][0];
+      
+        for (let i = 1, len=arr.length; i < len; i++) {
+          let v = arr[i][0];
+          let b = arr[i][1];
+          minX = (v < minX) ? v : minX;
+          maxX = (v > maxX) ? v : maxX;
+          minY = (b < minY) ? b : minY;
+          maxY = (b > maxY) ? b : maxY;
+        }
+      
+        return {'x':[minX, maxX],'y':[minY,maxY]};
+      }
+
 
     useEffect(() => {
       
@@ -30,7 +73,7 @@ export const Area = (props) => {
         
         if(item.name === pic.name && pic !== ''){
             
-            name.push({name: new Path2D()})
+            name.push({"name": new Path2D(),"coords":item.coords.coordinates})
             num+=1
             context.beginPath(name[num].name)
             context.fillStyle = color
@@ -52,25 +95,17 @@ export const Area = (props) => {
         setfirst(name)
     
     })
+
+       
+
         contextRef.current = context
     }, [pic,contours,scale])
     
-    if(document.getElementById("canvas") !==null){document.getElementById("canvas").addEventListener('click', function(event) {
-        event = event || window.event;
-        var ctx = document.getElementById("canvas").getContext("2d")
-        
-        for (var i = first.length - 1; i >= 0; i--){  
-        
-        if (first[i].name && ctx.isPointInPath(first[i].name, event.offsetX, event.offsetY)) {
-        
-            alert(event.clientX + ' : ' + event.clientY)
 
-        return
-        } 
-    }
-    })}
+
+
 
     return (
-    <canvas id='canvas'  ref={canvasRef}></canvas>
+    <canvas id='canvas' onMouseDown={addEvent}  ref={canvasRef}></canvas>
   )
 }
